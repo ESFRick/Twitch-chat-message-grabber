6b2ea8bec50738f67f1f3d459ddafc976b3f9910
ESFR <157487086+ESFRick@users.noreply.github.com>
2024-10-30 14:07:33 +0500

    Update main.py

--- main.py
+++ main.py
@@ -3,8 +3,8 @@ from twitchio.ext import commands
 class Bot(commands.Bot):
 
     def __init__(self):
-        super().__init__(token='oauth:dopc45xngyq1a0nerpgl5jtsuhwk3p', client_id='i9kv0o5u4mcvjld64wcymu3oxx63hu', nick='not_esfr', prefix='!',
-                         initial_channels=['JoinTheSystemm']) #Put here your channels to gather from
+        super().__init__(token='oauth:insert your oauth token', client_id='insert your client_id', nick='insert your twitch account nickname', prefix='!',
+                         initial_channels=['Put here your channels to gather from'])
 
     async def event_message(self, message):
         print(message.content, "|", message.channel.name)
